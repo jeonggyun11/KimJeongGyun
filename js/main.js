b3b0333896c4b2f61f34bfc02f407391f8dbdd75
jeonggyun11 <wjdrbs9656@naver.com>
2025-06-24 17:29:52 +0900

    0624 up

--- js/main.js
+++ js/main.js
@@ -184,3 +184,17 @@
   });
 
 })();
+
+// hero type
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
